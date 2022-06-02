d449318c360757b1546f24a2847a877c1264e521
Matheus Fernando <matheus.fernando.coder@gmail.com>
2022-06-02 20:26:39 -0300

    First steps with golang

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,14 @@
+package main //Programas executáveis iniciam pelo pacote main
+
+import "fmt"
+
+/*
+Os códigos em Go são organizados em pacotes
+e pra usá-los é necessário declarar um ou vários imports.
+*/
+
+//O problema era o p de print. Print deve estar com letra maiúscula.
+
+func main() {
+	fmt.Println("Olá, GO, o Matheus está à caminho!")
+}
